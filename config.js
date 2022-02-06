abb702d36997c7bf47956a71cc2979fa28be901b
Rom Rom <romrom@mitsi.digital>
2022-02-07 07:03:25 +0800

    Updated for setting up config.xml

---
+++ config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const fs = require('fs');
+
+async function main() {
+
+}
+
+function loadCordovaConfigXml() {
+
+}
+
+function interpolateConfigXml() {
+
+}
+
+function writeCordovaConfigXml() {
+
+}
+
+main().catch(console.error);
